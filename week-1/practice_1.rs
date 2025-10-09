d5381ff34905f521f5b059537044cb0ee67ddf14
user <user@users-MacBook-Pro.local>
2025-10-08 23:41:19 -0700

    Fix practice_1.rs syntax errors and add main function

--- week-1/practice_1.rs
+++ week-1/practice_1.rs
@@ -1,5 +1,11 @@
   fn main() {
        println!("Welcome to C0S 101");
   }
+       println!("The course learning outcome is as follows");
 
-  
+       println!("1. Distinguish between computer and computer programming,/n
+                 2. Develop some techniques in computer science,/n
+                 3. Understand the different areas of study in computer science,/n
+                 4. Conservant with applications of comuter science,/n
+                 5. Navigate the career prospects in computer scienc./n
+                 6. Conservant with computer programming concept.");
---
+++ week-2/practice_1.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("The course learning outcome is as follows");
+    println!("1. Distinguish between computer and computer programming");
+    println!("2. Develop some techniques in computer science");
+    println!("3. Understand the different areas of study in computer science");
+    println!("4. Conversant with applications of computer science");
+    println!("5. Navigate the career prospects in computer science");
+    println!("6. Conversant with computer programming concepts");
+}
