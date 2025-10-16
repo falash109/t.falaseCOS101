095490a13393af2ae07c1425a23196712473a6dc
user <user@users-MacBook-Pro.local>
2025-10-15 23:50:19 -0700

    practice_9

---
+++ week-3/practice_9/target/debug/deps/practice_9-6ecfb037c711a2ba.d
@@ -0,0 +1,5 @@
+/Users/user/t.falaseCOS101/week-3/practice_9/target/debug/deps/practice_9-6ecfb037c711a2ba.d: src/main.rs
+
+/Users/user/t.falaseCOS101/week-3/practice_9/target/debug/deps/practice_9-6ecfb037c711a2ba: src/main.rs
+
+src/main.rs:
---
+++ week-3/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+/Users/user/t.falaseCOS101/week-3/practice_9/target/debug/practice_9: /Users/user/t.falaseCOS101/week-3/practice_9/src/main.rs
