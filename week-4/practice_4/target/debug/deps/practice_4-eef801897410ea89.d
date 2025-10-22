3651f0cd19af77d4483e69418e1c57238c024934
falash109 <tisefalase11@gmail.com>
2025-10-22 12:16:40 -0700

    Delete week-4/practice_4 directory

--- week-4/practice_4/target/debug/deps/practice_4-eef801897410ea89.d
+++
@@ -1,5 +0,0 @@
-/Users/user/t.falaseCOS101/week-4/practice_4/target/debug/deps/practice_4-eef801897410ea89.d: src/main.rs
-
-/Users/user/t.falaseCOS101/week-4/practice_4/target/debug/deps/practice_4-eef801897410ea89: src/main.rs
-
-src/main.rs:
--- week-4/practice_4/target/debug/practice_4.d
+++
@@ -1,1 +0,0 @@
-/Users/user/t.falaseCOS101/week-4/practice_4/target/debug/practice_4: /Users/user/t.falaseCOS101/week-4/practice_4/src/main.rs
