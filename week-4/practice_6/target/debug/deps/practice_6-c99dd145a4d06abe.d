4d6b81f93f6a4bcdf47ce2f85431f694bad0e674
user <user@users-MacBook-Pro.local>
2025-10-23 13:07:53 -0700

    My week 4 practice 6

---
+++ week-4/practice_6/target/debug/deps/practice_6-c99dd145a4d06abe.d
@@ -0,0 +1,5 @@
+/Users/user/t.falaseCOS101/week-4/practice_6/target/debug/deps/practice_6-c99dd145a4d06abe.d: src/main.rs
+
+/Users/user/t.falaseCOS101/week-4/practice_6/target/debug/deps/practice_6-c99dd145a4d06abe: src/main.rs
+
+src/main.rs:
