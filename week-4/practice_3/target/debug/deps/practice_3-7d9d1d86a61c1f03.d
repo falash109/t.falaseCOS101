7012dc17544fd7f6d3a0cbd85fb3668fd4807e4a
user <user@users-MacBook-Pro.local>
2025-10-22 19:56:36 -0700

    my week 4 practice 3

---
+++ week-4/practice_3/target/debug/deps/practice_3-7d9d1d86a61c1f03.d
@@ -0,0 +1,5 @@
+/Users/user/t.falaseCOS101/week-4/practice_3/target/debug/deps/practice_3-7d9d1d86a61c1f03.d: src/main.rs
+
+/Users/user/t.falaseCOS101/week-4/practice_3/target/debug/deps/practice_3-7d9d1d86a61c1f03: src/main.rs
+
+src/main.rs:
