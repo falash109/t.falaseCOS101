52d9caab454e1b044af5f14362bced11ff14d050
user <user@users-MacBook-Pro.local>
2025-10-26 13:54:33 -0700

    1st project week 4

---
+++ week-4/project_1/target/debug/deps/project_1-468124af72ca2336.d
@@ -0,0 +1,5 @@
+/Users/user/t.falaseCOS101/week-4/project_1/target/debug/deps/project_1-468124af72ca2336.d: src/main.rs
+
+/Users/user/t.falaseCOS101/week-4/project_1/target/debug/deps/project_1-468124af72ca2336: src/main.rs
+
+src/main.rs:
