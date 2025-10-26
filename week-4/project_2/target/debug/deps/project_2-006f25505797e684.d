bcf998b613b25fb3a1d7b3fa23466deb032c737a
user <user@users-MacBook-Pro.local>
2025-10-26 14:30:32 -0700

    project 2 of week 4

---
+++ week-4/project_2/target/debug/deps/project_2-006f25505797e684.d
@@ -0,0 +1,5 @@
+/Users/user/t.falaseCOS101/week-4/project_2/target/debug/deps/project_2-006f25505797e684.d: src/main.rs
+
+/Users/user/t.falaseCOS101/week-4/project_2/target/debug/deps/project_2-006f25505797e684: src/main.rs
+
+src/main.rs:
