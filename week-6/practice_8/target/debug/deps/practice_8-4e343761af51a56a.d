33be95865d80507cbaeabc9ec46ba480d09aef5b
user <user@users-MacBook-Pro.local>
2025-11-05 21:00:32 -0800

    Week 6 commit eight

---
+++ week-6/practice_8/target/debug/deps/practice_8-4e343761af51a56a.d
@@ -0,0 +1,5 @@
+/Users/user/t.falaseCOS101/week-6/practice_8/target/debug/deps/practice_8-4e343761af51a56a.d: src/main.rs
+
+/Users/user/t.falaseCOS101/week-6/practice_8/target/debug/deps/practice_8-4e343761af51a56a: src/main.rs
+
+src/main.rs:
