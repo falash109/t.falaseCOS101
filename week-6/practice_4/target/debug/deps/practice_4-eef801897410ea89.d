46c7d52aadf172eadc68240589a272a93aece91c
user <user@users-MacBook-Pro.local>
2025-11-05 19:12:49 -0800

    Week 6 fifth commit

---
+++ week-6/practice_4/target/debug/deps/practice_4-eef801897410ea89.d
@@ -0,0 +1,5 @@
+/Users/user/t.falaseCOS101/week-6/practice_4/target/debug/deps/practice_4-eef801897410ea89.d: src/main.rs
+
+/Users/user/t.falaseCOS101/week-6/practice_4/target/debug/deps/practice_4-eef801897410ea89: src/main.rs
+
+src/main.rs:
---
+++ week-6/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+/Users/user/t.falaseCOS101/week-6/practice_5/target/debug/practice_5: /Users/user/t.falaseCOS101/week-6/practice_5/src/main.rs
