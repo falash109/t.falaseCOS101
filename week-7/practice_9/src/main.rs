64f015c59f42ab2faee084de21ca5d52f818cfc3
user <user@users-MacBook-Pro.local>
2025-11-12 16:39:27 -0800

    Ninth commit of week 7

---
+++ week-7/practice_9/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // The iter() function fetches values of all elements in an array.
+    let arr:[i32; 4] = [10, 20, 30, 40];
+    println!("Array is: {:?}", arr);
+    println!("Array size is: {}", arr.len());
+
+    for val in arr.iter() {
+        println!("Value is: {}", val);
+    }
+}
