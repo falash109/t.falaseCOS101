b779a1382a2d07d07bf2cd9a49de5402cd309282
user <user@users-MacBook-Pro.local>
2025-11-19 04:16:56 -0800

    Third practice learning to use get() method

---
+++ week-8/practice_3/target/debug/deps/practice_34-10edded23705bf7f.d
@@ -0,0 +1,5 @@
+/Users/user/t.falaseCOS101/week-8/practice_3/target/debug/deps/practice_34-10edded23705bf7f.d: src/main.rs
+
+/Users/user/t.falaseCOS101/week-8/practice_3/target/debug/deps/practice_34-10edded23705bf7f: src/main.rs
+
+src/main.rs:
---
+++ week-8/practice_3/target/debug/practice_34.d
@@ -0,0 +1,1 @@
+/Users/user/t.falaseCOS101/week-8/practice_3/target/debug/practice_34: /Users/user/t.falaseCOS101/week-8/practice_3/src/main.rs
