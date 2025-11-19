7dcbcae73b979c9b7d545c18f237014f58eac8ce
user <user@users-MacBook-Pro.local>
2025-11-19 14:31:03 -0800

    Practice seven - tuples

---
+++ week-8/practice_7/target/debug/deps/practice_7-1cde774feb4b480a.d
@@ -0,0 +1,5 @@
+/Users/user/t.falaseCOS101/week-8/practice_7/target/debug/deps/practice_7-1cde774feb4b480a.d: src/main.rs
+
+/Users/user/t.falaseCOS101/week-8/practice_7/target/debug/deps/practice_7-1cde774feb4b480a: src/main.rs
+
+src/main.rs:
