3bad8a405ed0026df8e26e72738fbc85065dd2b8
user <user@users-MacBook-Pro.local>
2025-11-26 12:12:03 +0100

    Practice 2, Reading from af ile

---
+++ week-9/practice_2/target/debug/deps/practice_2-a5088c267417c5bd.d
@@ -0,0 +1,5 @@
+/Users/user/t.falaseCOS101/week-9/practice_2/target/debug/deps/practice_2-a5088c267417c5bd.d: src/main.rs
+
+/Users/user/t.falaseCOS101/week-9/practice_2/target/debug/deps/practice_2-a5088c267417c5bd: src/main.rs
+
+src/main.rs:
